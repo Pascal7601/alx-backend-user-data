d273f76122b91b3ec89b2b19a5b86716739c90b6
Pascal7601 <levispascal63@gmail.com>
2024-08-30 00:22:17 +0300

    remove blank lines

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -5,12 +5,11 @@ encrypting your password
 
 import bcrypt
 
-def hash_password(password: str)-> str:
+
+def hash_password(password: str) -> str:
     pass_bytes = password.encode('utf-8')
     return bcrypt.hashpw(pass_bytes, bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
-
-print(is_valid(b'$2b$12$I9bRwLQABuIcHW5XLSXikuWlYKQhQcWwJ2ehrX75QaBCVdF3B1BLC', 'pascal'))
