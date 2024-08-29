a3186e479ce6f1e1cd4197a450740ebc1de13bac
Pascal7601 <levispascal63@gmail.com>
2024-08-30 00:18:12 +0300

    encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+encrypting your password
+"""
+
 import bcrypt
 
 def hash_password(password: str)-> str:
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+"""
+work on logging module
+"""
+
 from typing import List, Tuple
 import re
 import logging
