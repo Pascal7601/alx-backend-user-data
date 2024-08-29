4c377a592a932702e9c09c66bacc15eefc289b72
Pascal7601 <levispascal63@gmail.com>
2024-08-30 00:24:04 +0300

    adding comments to functions

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,9 +7,15 @@ import bcrypt
 
 
 def hash_password(password: str) -> str:
+    """
+    adds salt to a password and hashes it
+    """
     pass_bytes = password.encode('utf-8')
     return bcrypt.hashpw(pass_bytes, bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    checks the validity of a password
+    """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
