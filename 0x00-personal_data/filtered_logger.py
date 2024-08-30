f9eda2b0cdffc91efe38970283bacfd34e6b3cdd
Pascal7601 <levispascal63@gmail.com>
2024-08-30 14:44:22 +0300

    connecting to a database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,24 +6,35 @@ work on logging module
 from typing import List, Tuple
 import re
 import logging
+import mysql.connector
+from mysql.connector import Error
+import os
+from dotenv import load_dotenv
 
-PII_FIELDS: Tuple[str] = ("ssn", "password", "user_agent", "ip", "phone")
+load_dotenv()
+
+
+
+PII_FIELDS: Tuple[str] = ("ssn", "password", "email", "ip", "phone")
+
 
 def filter_datum(
         fields: List[str],
         redaction: str,
-        messages: str,
+        message: str,
         separator: str
         ):
     """
     returns the log message obfuscated:
     """
     for field in fields:
-        message = re.sub(f'{field}=.*?{separator}', f'{field}={redaction}{separator}', f'{messages}')
-        return message
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
 
 class RedactingFormatter(logging.Formatter):
-    """ 
+    """
     Redacting Formatter class
     """
 
@@ -39,12 +50,59 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
 
+
 def get_logger() -> logging.Logger:
+    """
+    create a logger object
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(RedactingFormatter(PII_FIELDS))
     logger.addHandler(stream_handler)
     return logger
+
+
+def get_db():
+    """
+    connect to the database
+    """
+    connection = {
+        'user': os.getenv('PERSONAL_DATA_DB_USERNAME'),
+        'password': os.getenv('PERSONAL_DATA_DB_PASSWORD'),
+        'host': os.getenv('PERSONAL_DATA_DB_HOST'),
+        'port': 3308,
+        'use_pure': True,
+        'ssl_disabled': True
+    }
+
+    try:
+        db = mysql.connector.connect(**connection)
+        print("Connection successful:", db)
+        return db
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+        return None
+
+
+def main():
+    STATEMENT = """
+    SELECT * FROM users;
+"""
+    conn = get_db()
+
+    cursor = conn.cursor()
+    cursor.execute("USE my_db")
+    cursor.execute(STATEMENT)
+    users = cursor.fetchall()
+    for user in users:
+        get_logger().info(user)
+        print(user)
+
+    cursor.close()
+    conn.close()
+
+main()
+
 
 
