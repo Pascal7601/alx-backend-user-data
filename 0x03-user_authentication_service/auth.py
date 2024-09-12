9ca74c918fbce67339e1b2398c21b4b72b04b3a8
Pascal7601 <levispascal63@gmail.com>
2024-09-12 07:25:43 -0700

    add user with hasshed password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,8 @@
 file to handle password encryption
 """
 import bcrypt
+from user import User
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
@@ -11,3 +13,25 @@ def _hash_password(password: str) -> bytes:
     """
     encoded_pwd = password.encode('utf-8')
     return bcrypt.hashpw(encoded_pwd, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        register or add a user to our datbase
+        """
+        if not email or not password:
+            raise ValueError("provide both email and password")
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except:
+            hash_pwd = _hash_password(password)
+            new_user = self._db.add_user(email, hash_pwd)
+            return new_user
