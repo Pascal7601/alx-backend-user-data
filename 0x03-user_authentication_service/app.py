cfe5ce87a1f2589157b0e577f5ab61992d527e4a
Pascal7601 <levispascal63@gmail.com>
2024-09-12 09:39:24 -0700

    add a post route for creating new users

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,8 +2,11 @@
 """
 a flask app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
+
+AUTH = Auth()
 
 app = Flask(__name__)
 
@@ -14,6 +17,21 @@ def home():
     index route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def add_user():
+    """
+    route to add a user to a db
+    """
+    email = request.form['email']
+    password = request.form['password']
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 201
+    except ValueError as e:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
